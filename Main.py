1f05ec33832eaee3c03b132833baaca96d7cef3d
Santiago <santiago.t.perozzi@gmail.com>
2026-06-05 16:49:50 -0300

    main opcion 4 ajustes

--- Main.py
+++ Main.py
@@ -25,7 +25,6 @@ while True:
         ordenar_listas(cod_deuda, det_deuda, categoria, monto_total, monton_pendiente, vencimiento, estado)
         print ("="*50)
         print("Informe: ")
-        print ("="*50)
         
         for i in range (len(cod_deuda)):
             print(f"Código: {cod_deuda[i]} | Detalle: {det_deuda[i]} | Categoria: {categoria[i]} | Monto Pendiente: ${monton_pendiente[i]} | Vencimiento: {vencimiento[i]} | Estado: {estado[i]}")
